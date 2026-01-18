35b6306be0c59453d32f4fd8583e78e665858d1e
JoannaVC <joanna2409019@hybridge.education>
2026-01-17 20:22:14 -0600

    Agregado cambios vistos en cuaderno 2.6 Pipelines

--- app.py
+++ app.py
@@ -1,28 +1,28 @@
 from flask import Flask, request, jsonify
 import pickle
-import numpy as np
-
-modelo = None
-
-# Cargar el modelo
-with open("modelo.pkl", 'rb') as file:
-    modelo = pickle.load(file)
+import pandas as pd
 
 app = Flask(__name__)
 
+# Cargar el modelo guardado
+with open('pipeline.pkl', 'rb') as archivo_modelo:
+    modelo = pickle.load(archivo_modelo)
+
 @app.route('/predecir', methods=['POST'])
-def predict():
-    # obtener json
-    data = request.get_json(force=True)
-    
-    # convertir los datos a un arreglo de Numpy
-    input_data = np.array(data['input']).reshape(1, -1)
-    
-    # Hacer predicción
+def predecir():
+    # Obtener los datos de la solicitud
+    data = request.get_json()
+
+    # Crear un DataFrame de pandas a partir del JSON
+    input_data = pd.DataFrame([data])
+
+    # Hacer la predicción usando el modelo que tiene el pipeline que hará la transformación
     prediccion = modelo.predict(input_data)
     
-    # regresar predicción en formato json
-    return jsonify({'prediccion': int(prediccion[0])})
+    # Devolver la predicción como JSON
+    output = {'Survived': int(prediccion[0])}
+    
+    return jsonify(output)
 
 if __name__ == '__main__':
     app.run(debug=True)
